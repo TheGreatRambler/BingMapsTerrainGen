449a3bc78b8382695f94c165ef7140e43a2564ec
TheGreatRambler <TheGreatRambler>
2020-09-28 00:39:54 -0400

    Attempt beginning code

---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <rapidjson/document.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/stringbuffer.h>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	std::string url = "https://dev.virtualearth.net/REST/v1/Locations?key={mapSession.DeveloperKey}";
+	return 0;
+}
